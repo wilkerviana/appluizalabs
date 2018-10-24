5db5ac5256725b44d45da9c4154aefeeb66523b8
Wilker Viana <wilker.info@gmail.com>
2018-10-24 03:16:00 -0300

    Fix configs to deploy

--- src/server.js
+++ src/server.js
@@ -5,17 +5,11 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist/appluizalabs"));
 
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname + "/dist/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/appluizalabs/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000, function() {
-  console.log(
-    "Express server listening on port %d in %s mode",
-    this.address().port,
-    app.settings.env
-  );
-});
+app.listen(process.env.PORT || 8080);
