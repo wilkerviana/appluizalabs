07a5acee9829be4b747b8497f99159610673f67f
Wilker Viana <wilker.info@gmail.com>
2018-11-08 06:12:45 -0200

    Fix component Barcode scanner

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, VERSION, ViewChild } from "@angular/core";
+import { Result } from "@zxing/library";
+import { ZXingScannerComponent } from "@zxing/ngx-scanner";
 
 @Component({
   selector: "app-home",
@@ -8,7 +10,72 @@ import { Component, OnInit } from "@angular/core";
 export class HomeComponent implements OnInit {
   panelOpenState: boolean;
 
+  ngVersion = VERSION.full;
+
   constructor() {}
 
-  ngOnInit() {}
+  @ViewChild("scanner")
+  scanner: ZXingScannerComponent;
+
+  hasDevices: boolean;
+  hasPermission: boolean;
+  qrResultString: string;
+  qrResult: Result;
+
+  availableDevices: MediaDeviceInfo[];
+  currentDevice: MediaDeviceInfo;
+
+  ngOnInit(): void {
+    this.scanner.camerasFound.subscribe((devices: MediaDeviceInfo[]) => {
+      this.hasDevices = true;
+      this.availableDevices = devices;
+
+      // selects the devices's back camera by default
+      // for (const device of devices) {
+      //     if (/back|rear|environment/gi.test(device.label)) {
+      //         this.scanner.changeDevice(device);
+      //         this.currentDevice = device;
+      //         break;
+      //     }
+      // }
+    });
+
+    this.scanner.camerasNotFound.subscribe(() => (this.hasDevices = false));
+    this.scanner.scanComplete.subscribe(
+      (result: Result) => (this.qrResult = result)
+    );
+    this.scanner.permissionResponse.subscribe(
+      (perm: boolean) => (this.hasPermission = perm)
+    );
+  }
+
+  displayCameras(cameras: MediaDeviceInfo[]) {
+    console.debug("Devices: ", cameras);
+    this.availableDevices = cameras;
+  }
+
+  handleQrCodeResult(resultString: string) {
+    console.debug("Result: ", resultString);
+    this.qrResultString = resultString;
+  }
+
+  onDeviceSelectChange(selectedValue: string) {
+    console.debug("Selection changed: ", selectedValue);
+    this.currentDevice = this.scanner.getDeviceById(selectedValue);
+  }
+
+  stateToEmoji(state: boolean): string {
+    const states = {
+      // not checked
+      undefined: "❔",
+      // failed to check
+      null: "⭕",
+      // success
+      true: "✔",
+      // can't touch that
+      false: "❌"
+    };
+
+    return states["" + state];
+  }
 }
